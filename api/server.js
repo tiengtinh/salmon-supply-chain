5ae815e701280b37928163cf16ad61be7027c843
Tinh Ngo <tiengtinh@gmail.com>
2018-04-07 00:48:13 +0700

    endpoint to record salmon

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 
+const invoke = require('./invoke-transaction')
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -10,6 +12,38 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world\n');
+});
+
+app.get('/salmons', (req, res) => {
+
+})
+
+app.post('/salmons', async (req, res) => {
+  logger.debug('username :' + req.username);
+  logger.debug('orgname:' + req.orgname);
+  
+  const {
+    id,
+    vessel,
+    datetime,
+    location,
+    holder,
+  } = req.body
+
+  const peers = [] // Optional. The peers that will receive this request, when not provided the list of peers added to this channel object will be used.
+  const channelName = 'transfers'
+  const chaincodeName = 'salmon'
+  const fcn = "recordSalmon"
+  const args = [
+    id,
+    vessel,
+    datetime,
+    location,
+    holder,
+  ]
+
+  const message = await invoke.invokeChaincode(peers, channelName, chaincodeName, fcn, args, req.username, req.orgname);
+	res.send(message);
 });
 
 app.listen(PORT, HOST);
